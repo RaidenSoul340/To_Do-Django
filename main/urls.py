219796075a4f2992309aa4a861f4a2937d041c03
lvfwk <joaorodrigooliver9@gmail.com>
2024-10-30 21:20:40 -0300

    atualizando o delete e edicao
    adicionando atualizacao e delete em funcioarios e tasks

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='lista_tarefas'),
     path('Funcionarios/', views.ListaFuncionario, name='Lista_funcionarios'),
+    #================================================================================#
     path('addFuncionarios/', views.add_funcionario, name='add_funcionario'),
     path('add/', views.add_task, name='add_task'),
-#    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    #================================================================================#
+    path('delete/task/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('delete/funcionario/<int:funcionario_id>/', views.delete_funcionario, name='delete_funcionario'),
+    #================================================================================#
+    path('atualizar/<int:id>/', views.atualizar_tasks, name='atualizar_task'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task, Funcionarios
 
 #Exibir informações no front
@@ -7,6 +7,11 @@ def task_list(request):
 
     return render(request, 'tasks/task_list.html', {'tasks':tasks})
 
+def ListaFuncionario(request):
+    funcionarios_on = Funcionarios.objects.filter(presente=True)
+    funcionarios_off = Funcionarios.objects.filter(presente=False)
+    return render(request, 'funcionarios/funciorios_lista.html', {'funcionarios_on':funcionarios_on, 'funcionarios_off':funcionarios_off})
+#================================================================================#
 def add_task(request):
     if request.method == "POST":
         titulo = request.POST.get('titulo')
@@ -23,17 +28,6 @@ def add_task(request):
         return redirect('lista_tarefas')
     return render(request, 'tasks/add_task.html')
 
-def delete_task(task_id):
-    task = Task.objects.get(id=task_id)
-    task.delete()
-    return redirect('task_list')
-
-#================================================================================#
-def ListaFuncionario(request):
-    funcionarios_on = Funcionarios.objects.filter(presente=True)
-    funcionarios_off = Funcionarios.objects.filter(presente=False)
-    return render(request, 'funcionarios/funciorios_lista.html', {'funcionarios_on':funcionarios_on, 'funcionarios_off':funcionarios_off})
-
 def add_funcionario(request):
     if request.method == "POST":
         Nome = request.POST.get('nome')
@@ -49,6 +43,52 @@ def add_funcionario(request):
 
         return redirect('Lista_funcionarios')
     return render(request, 'funcionarios/add_funcionarios.html')
+#================================================================================#
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('lista_tarefas')
 
+def delete_funcionario(request, funcionario_id):
+    funcionario = get_object_or_404(Funcionarios, id=funcionario_id)
+    funcionario.delete()
+    return redirect('Lista_funcionarios')
+#================================================================================#
+def atualizar_tasks(request, id):
+    atualizar = get_object_or_404(Task, id=id)
+
+    if request.method == "POST":
+
+        atualizar.titulo = request.POST.get('titulo')
+        atualizar.descricao = request.POST.get('descricao')
+        atualizar.status = request.POST.get('status')
+
+        if atualizar.status:
+            atualizar.status = True
+        else:
+            atualizar.status = False
+
+        atualizar.save()
+
+        return redirect('lista_tarefas')
+    return render(request, 'tasks/atualizar_tarefas.html', {'tasks':atualizar})
+
+def atualizar_funcionario(request, id):
+    atualizar = get_object_or_404(Funcionarios, id=id)
+
+    if request.method == 'POST':
+        atualizar.Nome = request.POST.get('nome')
+        atualizar.funcao = request.POST.get('funcao')
+        atualizar.presente = request.POST.get('presente')
+    
+        if atualizar.presente:
+            atualizar.presente = True
+        else:
+            atualizar.presente = False
+        
+        atualizar.save()
+        
+        return redirect
+#================================================================================#
 
 
